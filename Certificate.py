d72ac26fbebbf207db6dd29dcce63dc83186b3bd
jvs-alpha <jvsamuel.rufus@gmail.com>
2020-01-15 15:00:48 +0530

    changed mailService, changed Position, Certificarte

--- Certificate.py
+++ Certificate.py
@@ -1,25 +1,22 @@
 import cv2
 import os
 from PIL import Image
-import numpy as np
 import json
 import sys
 
 
-def Certificate(image,posfile,name):
+def Certificate(image,posfile,name,id):
     img = cv2.imread(image)
     json_file = open(posfile,"r+")
     pos = json.load(json_file)
-    cv2.putText(img,name,(pos["name"]["x"],pos["name"]["y"]),cv2.FONT_HERSHEY_TRIPLEX,2,(0,0,0),2,cv2.LINE_AA)
-    #cv2.imwrite("images/test.jpg",img)
-    path = os.getcwd()
+    cv2.putText(img,"NAME : {}".format(name),(pos["name"]["x"],pos["name"]["y"]),cv2.FONT_HERSHEY_TRIPLEX,2,(0,0,0),2,cv2.LINE_AA)
     img = Image.fromarray(img)
     img = img.convert("RGB")
-    img.save(r"{}/images/test.pdf".format(path))
+    img.save(r"{}/images/{}.pdf".format(os.getcwd(),id))
 
 if __name__ == "__main__":
-    if(len(sys.argv) < 4):
+    if(len(sys.argv) < 5):
         print("Try:")
-        print("python3 Certificate.py <image> <position file> <name>")
+        print("python3 Certificate.py <image> <position file> <name> <Register_ID>")
         sys.exit(0)
-    Certificate(sys.argv[1],sys.argv[2],sys.argv[3])
+    Certificate(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
--- MailService.py
+++ MailService.py
@@ -1,21 +1,33 @@
 import smtplib
+from email.message import EmailMessage
 import sys
+import os
 
-def send_mail(sender,recv,passwd,msg):
+def send_mail(recv,id):
     try:
-        mail = smtplib.SMTP("smtp.gmail.com",587)
-        mail.starttls()
-        mail.login(sender,passwd)
-        mail.sendmail(sender,recv,msg)
+        msg = EmailMessage()
+        msg["Subject"] = "test"
+        msg["From"] = os.environ.get("EMAIL_USER")
+        msg["To"] = recv
+        msg.set_content("PDF")
+        path = os.path.join(os.getcwd(),"images/")
+        file = "{}{}.pdf".format(path,id)
+        f = open(file,"rb")
+        file_data = f.read()
+        f.close()
+        msg.add_attachment(file_data,maintype="application",subtype="octet-stream",filename="{}.pdf".format(id))
+        mail = smtplib.SMTP_SSL("smtp.gmail.com",465)
+        mail.login(os.environ.get("EMAIL_USER"),os.environ.get("EMAIL_PASSWD"))
+        mail.send_message(msg)
         mail.quit()
     except SMTPException:
         print("mailing service not working")
 
 
 if __name__ == "__main__":
-    if(len(sys.argv) < 5):
+    if(len(sys.argv) < 3):
         print("not enough arguments")
         print("Try:")
-        print("python mailservice.py <sender_mail_id> <receiver_mail_id> <sender_password>")
+        print("python mailservice.py <receiver_email> <Register_ID>")
         sys.exit(0)
-    send_mail(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[4])
+    send_mail(sys.argv[1],sys.argv[2])
