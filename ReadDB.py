1a21e2df8b3971c25ccf934ff40a8524c8a023b3
jvs-alpha <jvsamuel.rufus@gmail.com>
2020-04-06 04:28:31 +0530

    added new method for parsing the arguments in python

--- ReadDB.py
+++ ReadDB.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import sys
+import argparse
 
 def ReadDB(db,table):
     cur = db.cursor()
@@ -10,13 +11,18 @@ def ReadDB(db,table):
     return data
 
 if __name__ == "__main__":
-    if len(sys.argv) < 4:
-        print("Usage: python3 ReadDB.py <user> <passwd> <table>")
-    else:
-        db = mysql.connector.connect(
+    parse = argparse.ArgumentParser(description="This is a Database Read Script")
+    parse.add_argument("User",type=str,help="This is the Database Username")
+    parse.add_argument("Password",type=str,help="This is the Database Password")
+    parse.add_argument("Table",type=str,help="This is the Database Table")
+    argv = parse.parse_args()
+
+    db = mysql.connector.connect(
         host="localhost",
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        database="nric")
-        for i in ReadDB(db,sys.argv[3]):
-            print(i)
+        user=argv.User,
+        passwd=argv.Password,
+        database="nric"
+        
+    )
+    for i in ReadDB(db,argv.Table):
+        print(i)
